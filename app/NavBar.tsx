dc214ae7f8b35f58961317d39a417818b98ce7c4
Valeriusdev <kakao757@gmail.com>
2025-02-16 19:57:29 -0800

    update NavBar component styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex">
+    <div className="flex bg-slate-200">
       <Link href="/" className="mr-5">
         NavBar
       </Link>
